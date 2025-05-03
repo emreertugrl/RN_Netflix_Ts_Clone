8d69bb642bd8b1d515060ba88b97f3c0b9b8efcb
emreertugrl <emrertugrl@gmail.com>
2025-05-03 19:45:21 +0300

    top rated movies and tv get to reducer

--- src/service/urls.ts
+++ src/service/urls.ts
@@ -1,6 +1,7 @@
 export const BASE_URL = 'https://api.themoviedb.org/3/';
 export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500/';
-export const TOP_RATED_URL = 'movie/top_rated';
+export const TOP_RATED_MOVIES_URL = 'movie/top_rated';
+export const TOP_RATED_TV_URL = 'tv/top_rated';
 export const POPULAR_MOVIES_URL = 'movie/popular';
 export const UPCOMING_MOVIES_URL = 'movie/upcoming';
 export const NOWPLAYING_MOVIES_URL = 'movie/now_playing';
