7ee47f9a2dcfbbf075513c47c17ea25a9a60024f
emreertugrl <emrertugrl@gmail.com>
2025-05-03 20:41:11 +0300

    popular tv and movies url defined

--- src/service/urls.ts
+++ src/service/urls.ts
@@ -3,6 +3,7 @@ export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500/';
 export const TOP_RATED_MOVIES_URL = 'movie/top_rated';
 export const TOP_RATED_TV_URL = 'tv/top_rated';
 export const POPULAR_MOVIES_URL = 'movie/popular';
+export const POPULAR_TV_URL = 'tv/popular';
 export const UPCOMING_MOVIES_URL = 'movie/upcoming';
 export const NOWPLAYING_MOVIES_URL = 'movie/now_playing';
 export const MOVIE_URL = `movie/`;
